a6690fb4b9c543fecaf93550fd6da18a943e4731
henryvargasguevara <henryvargasguevara@gmail.com>
2018-06-05 22:27:12 -0500

    Create Carpeta.java

---
+++ ProyModRecaudacion/src/main/java/com/peruviansy/model/Carpeta.java
@@ -0,0 +1,38 @@
+package com.peruviansy.model;
+
+public class Carpeta {
+	
+  int id;
+  String nombre;
+  String url;
+  long tamano;
+  
+  
+  
+public long getTamano() {
+	return tamano;
+}
+public void setTamano(long tamano) {
+	this.tamano = tamano;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getUrl() {
+	return url;
+}
+public void setUrl(String url) {
+	this.url = url;
+}
+  
+  
+}
